8fb579c09a09bdbd24db2986ef2af9b843ea4e09
Refiloe1994 <mphosiref@gmail.com>
2024-08-12 22:47:01 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# Get the user's name
+user_name = input("Please enter your name: ")
+
+# Print the name
+print("Hello, " + user_name )
+
+# Get the user's age
+user_age = input("Please enter your age: ")
+
+# Print the age
+print("Your age is:", user_age)
+
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,14 @@
+# Define menu items
+menu = {"soda", "fries", "burger", "chicken strips", "chicken nuggets" }
+
+# Prompt for user input
+item1 = input("What is your first item of choice? :")
+item2 = input("What is your second item of choice? :")
+item3 = input("What is your third item of choice? :")
+
+print("Order confirmation! You have ordered:")
+print("item1")
+print("item2")
+print("item3")
+
+
